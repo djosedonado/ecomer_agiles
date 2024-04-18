27315a3896a5ad24785d6b34fbbe47952a32d9f3
djosedonado <djosedonado@unicesar.edu.co>
2024-04-18 11:19:20 -0500

    arraeglos
    Signed-off-by: djosedonado <djosedonado@unicesar.edu.co>

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,44 +1,33 @@
 "use client";
 
-const RegisterUser = () => {
+const Login = () => {
   return (
     <div className="container mx-auto">
       <div className="flex justify-center">
-        <div className="w-full lg:w-1/2 md:w-2/3">
+        <div className="w-full lg:w-2/3">
           <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div className="grid place-items-center mb-4">
-              <div className="w-20 h-20 rounded-full bg-gray-200"></div>
+              <div className="w-20 h-20 rounded-full bg-gray-200">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-12 h-12 mx-auto my-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                  />
+                </svg>
+              </div>
             </div>
             <h1 className="text-center text-xl text-blue-500 mb-4">
-              Registro de Usuario
+              Ingrese sus usuario
             </h1>
             <form>
-              <div className="mb-4">
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                >
-                  Nombre
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="nombre"
-                  type="text"
-                  placeholder="Nombre"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                >
-                  Apellido
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="apellido"
-                  type="text"
-                  placeholder="Apellido"
-                />
-              </div>
               <div className="mb-4">
                 <label
                   className="block text-gray-700 text-sm font-bold mb-2"
@@ -69,13 +58,13 @@ const RegisterUser = () => {
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="button"
               >
-                Registrar
+                INGRESAR
               </button>
             </form>
             <p className="text-center text-sm mt-4">
-              ¿Ya tienes una cuenta?{" "}
+              ¿No tienes cuenta?{" "}
               <a href="#" className="text-blue-500">
-                Logueate
+                Regístrate
               </a>
             </p>
           </div>
@@ -85,4 +74,4 @@ const RegisterUser = () => {
   );
 };
 
-export default RegisterUser;
+export default Login;
